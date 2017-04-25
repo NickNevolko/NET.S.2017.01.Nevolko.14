96ba41e5106958690aea49d83e0b8f833e1d8a53
nicknevolko <nikolay.nevolko@gmail.com>
2017-04-26 00:38:49 +0300

    added task1

---
+++ Task1_Fibonacci/Task1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_Fibonacci
+{
+    /// <summary>
+    /// static class that contains a method for the representation a Fibonacci sequence
+    /// </summary>
+    public static class Task1
+    {
+        /// <summary>
+        /// represents a Fibonacci sequence
+        /// </summary>
+        /// <param name="n">indicates how much elements of sequence needs</param>
+        /// <returns>N elements of the Fibonacci</returns>
+        public static IEnumerable<int> Fibonacci(int n)
+        {
+            if (n < 0) throw new ArgumentOutOfRangeException(nameof(n));
+            int item1 = 1;
+            int item2 = 1;
+            if (n == 1) yield return item1;
+            else if (n == 2)
+            {
+                yield return item2;
+                yield return item1;
+            }
+            else
+            {
+                yield return item1;
+                for (int i = 2; i <= n; i++)
+                {
+                    int buff = item2;
+                    item2 += item1;
+                    item1 = buff;
+                    yield return item1;
+                }
+            }
+        }
+    }
+}
---
+++ Task1_Tests/TestClass.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Task1_Fibonacci.Task1;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        int[] arr1 = { 1, 1, 2, 3, 5 };
+        int[] arr2 = { 1};
+        int[] arr3 = { 1, 1};
+        int[] arr4 = { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144};
+        [Test]
+        public void TestMethod()
+        {
+            Assert.AreEqual(arr1, Fibonacci(5));
+            Assert.AreEqual(arr2, Fibonacci(1));
+            Assert.AreEqual(arr3, Fibonacci(2));
+            Assert.AreEqual(arr4, Fibonacci(12));
+        }
+    }
+}
